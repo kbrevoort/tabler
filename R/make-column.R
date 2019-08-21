a61babe9ae1b3e37aebe1758baf9e365d21ee0f3
Ken Brevoort <ken@brevoort.com>
2019-08-21 12:04:44 -0400

    Construct xlevels for factor variables in felm
    Exisiting code was creating xlevels list only for the variables used as fixed effects.  Other factor (or logical variables) were being ignored.

--- R/make-column.R
+++ R/make-column.R
@@ -42,11 +42,17 @@ make_column.felm <- function(in_result) {
   col_obj <- list()
   attr(col_obj, 'class') <- 'tabler_column'
   col_obj$dep_var <- in_result$lhs
-  col_obj$var_names <- c(attributes(in_result$terms)$term.labels,
-                         names(in_result$fe))
+  var_names <- c(attributes(in_result$terms)$term.labels,
+                 names(in_result$fe))
+  col_obj$var_names <- var_names
   col_obj$est_type <- class(in_result)[1]
-  col_obj$xlevels <- purrr::map(names(in_result$fe), ~ levels(in_result$fe[[.x]]))
-  names(col_obj$xlevels) <- names(in_result$fe)
+  
+  # Expanded xlevels creation -- previous method only included absorbed FEs, not
+  # factor variables as well.
+  fe_xlevels <- purrr::map(names(in_result$fe), ~ levels(in_result$fe[[.x]]))
+  names(fe_xlevels) <- names(in_result$fe)
+  fct_xlevels <- build_felm_factor_levels(setdiff(names(coef(in_result)), var_names), var_names)
+  col_obj$xlevels <- c(fe_xlevels, fct_xlevels)
 
   col_obj$absorbed_vars <- names(in_result$fe)
 
@@ -60,3 +66,15 @@ make_column.felm <- function(in_result) {
   col_obj
 }
 
+build_felm_factor_levels <- function(unaccounted, var_names) {
+  ret_val <- list()
+  for (nm in var_names) {
+    n <- nchar(nm)
+    if (any(substr(unaccounted, 1, n) == nm)) {
+      ret_val[[nm]] <- unaccounted[substr(unaccounted, 1, n) == nm] %>%
+        substring(n + 1)
+    }
+  }
+
+  if (length(ret_val) == 0) NULL else ret_val
+}
